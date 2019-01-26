6975a810c6e939518d60060869aff7918d19a02c
sumanth8ch <chsm17@student.bth.se>
2019-01-26 14:25:58 +0100

    final commit

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
 
-    def setUp(self):
-        main.app.testing = True
-        self.app = main.app.test_client()
+        def setUp(self):
+            main.app.testing = True
+            self.app = main.app.test_client()
 
         def test_addint(self):
             rv =  self.app.get('/add?A=2&B=5')
@@ -49,19 +49,6 @@ class MyTestCase(unittest.TestCase):
             rv =  self.app.get('/mul?A=2.3&B=-3.3')
             self.assertEqual(b'-7.59 \n', rv.data)
 
-        def test_divint(self):
-            rv =  self.app.get('/div?A=2&B=5')
-            self.assertEqual(b'0.4 \n', rv.data)
-            self.assertNotEqual(b'6.000 \n',rv.data)
-        def test_divfloat(self):
-            rv =  self.app.get('/div?A=2.3&B=3.3')
-            self.assertEqual(b'0.696969696969697 \n', rv.data)
-        def test_divfrac(self):
-            rv =  self.app.get('/div?A=2/3&B=3/3')
-            self.assertEqual(b'0.6666666666666666 \n', rv.data)
-        def test_divneg(self):
-            rv =  self.app.get('/div?A=2.3&B=-3.3')
-            self.assertEqual(b'-0.696969696969697 \n', rv.data)    
 
 if __name__ == '__main__':
     unittest.main()
